91bd87a6f9853c74d96369ec5bbc70b70a991106
Pavlo Baranchuk <pavlo.baranchuk@goreel.tech>
2026-05-13 21:33:36 +0300

    added write and read, bme280_set_mode, types of initializating, addreses

--- Drivers/bme280/bme280.c
+++ Drivers/bme280/bme280.c
@@ -7,75 +7,154 @@
 #include "bme280.h"
 #include "string.h"
 
+bme280_status_t bme280_init(bme280_t* dev, const bme280_bus_t* bus) {
+    if (!dev || !bus) {
+        return BME280_STATUS_INVALID_ARG;
+    }
+    if (!bus->read || !bus->write) {
+        return BME280_STATUS_INVALID_ARG;
+    }
+
+    uint8_t currentID = 0;
+    dev->status = BME280_STATUS_OK;
+    dev->bme280_bus = *bus;
+
+    if (bus->read(bus->dev_addr, BME280_ID_ADDR, &currentID, 1, bus->ctx) != BME280_STATUS_OK) {
+        return BME280_STATUS_ERROR;
+    }
+    if (currentID != BME280_ID) {
+        return BME280_STATUS_INVALID_ARG;
+    }
+
+    if (bus->write(bus->dev_addr, BME280_CTRL_MEAS_ADDR, (uint8_t)((uint8_t)dev->bme280_temperature_ovs | (uint8_t)dev->bme280_pressure_ovs | (uint8_t)dev->bme280_mode), 1, bus->ctx) != BME280_STATUS_OK) {
+        return BME280_STATUS_ERROR;
+    }
+    if (bus->write(bus->dev_addr, BME280_CTRL_HUM_ADDR, (uint8_t)dev->bme280_humidity_ovs, 1, bus->ctx) != BME280_STATUS_OK) {
+        return BME280_STATUS_ERROR;
+    }
+
+    return BME280_STATUS_OK;
+}
+
 bme280_status_t bme280_reset(bme280_t* dev) {
+    if (!dev || !dev->bme280_bus.write) {
+        return BME280_STATUS_INVALID_ARG;
+    }
+
+    bme280_bus_t bme280_bus = dev->bme280_bus;
+    memset(dev, 0, sizeof(*dev));
     dev->reset = BME280_RESET;
-    dev->bme280_humidity_ovs = 0;
-    dev->bme280_pressure_ovs = 0;
-    dev->bme280_temperature_ovs = 0;
+    dev->bme280_bus = bme280_bus;
+
+    if (dev->bme280_bus.write(dev->bme280_bus.dev_addr, BME280_RESET_ADDR, &BME280_RESET, 1, dev->bme280_bus.ctx) != BME280_STATUS_OK) {
+        return BME280_STATUS_ERROR;
+    }
+
+    return BME280_STATUS_OK;
+}
+
+bme280_status_t bme280_set_mode(bme280_t* dev, bme280_mode_t mode) {
+    if (!dev || !dev->bme280_bus.read || !dev->bme280_bus.write) {
+        return BME280_STATUS_INVALID_ARG;
+    }
+
+    bme280_bus_t* bus = &dev->bme280_bus;
+
+    uint8_t ctrlMeas = 0;
+    if (bus->read(bus->dev_addr, BME280_CTRL_MEAS_ADDR, &ctrlMeas, 1, bus->ctx) != BME280_STATUS_OK) {
+        return BME280_STATUS_ERROR;
+    }
+    ctrlMeas = ((ctrlMeas & 0xFC) | (mode & 0x03));
+    if (bus->write(bus->dev_addr, BME280_CTRL_MEAS_ADDR, ctrlMeas, 1, bus->ctx) != BME280_STATUS_OK) {
+        return BME280_STATUS_ERROR;
+    }
 
     return BME280_STATUS_OK;
 }
 
 bme280_status_t bme280_read_calibration(bme280_t* dev) {
-    memset(dev->_bme280HUMIRegister, 0, sizeof(dev->_bme280HUMIRegister));
-    memset(dev->_bme280PRESRegister, 0, sizeof(dev->_bme280PRESRegister));
-    memset(dev->_bme280TEMPRegister, 0, sizeof(dev->_bme280TEMPRegister));
-
-    size_t i = 0;
-    while(i < BME280_HUMIDITY_SIZE) {
-        dev->_bme280HUMIRegister[i] = (BME280_CALIB_HUMI_ADDR_START + i);
-        ++i;
+    if (!dev || !dev->bme280_bus.read || !dev->bme280_bus.write) {
+        return BME280_STATUS_INVALID_ARG;
     }
 
-    i = 0;
-    while(i < BME280_PRESS_SIZE) {
-        dev->_bme280PRESRegister[i] = (BME280_CALIB_PRES_ADDR_START + i);
-        ++i;
+    uint8_t len = BME280_CALIB_PRES_ADDR_END - BME280_CALIB_TEMP_ADDR_START + 1;
+    if (dev->bme280_bus.read(dev->bme280_bus.dev_addr, BME280_CALIB_TEMP_ADDR_START, dev->bme280_calib_buf, len, dev->bme280_bus.ctx) != BME280_STATUS_OK) {
+        dev->status = BME280_STATUS_ERROR;
+        return BME280_STATUS_ERROR;
     }
 
-    i = 0;
-    while(i < BME280_TEMPERATURE_SIZE) {
-        dev->_bme280TEMPRegister[i] = (BME280_CALIB_TEMP_ADDR_START + i);
-        ++i;
+
+    if (dev->bme280_bus.read(dev->bme280_bus.dev_addr, BME280_CALIB_HUMI_ADDR_START, (dev->bme280_calib_buf + len), BME280_CALIB_HUMI_SIZE_START, dev->bme280_bus.ctx) != BME280_STATUS_OK) {
+        dev->status = BME280_STATUS_ERROR;
+        return BME280_STATUS_ERROR;
+    }
+    len += BME280_CALIB_HUMI_SIZE_START;
+    if (dev->bme280_bus.read(dev->bme280_bus.dev_addr, BME280_CALIB_HUMI_ADDR_CONTINUE, (dev->bme280_calib_buf + len), (BME280_CALIB_HUMI_ADDR_END - BME280_CALIB_HUMI_ADDR_CONTINUE + 1), dev->bme280_bus.ctx) != BME280_STATUS_OK) {
+        dev->status = BME280_STATUS_ERROR;
+        return BME280_STATUS_ERROR;
+    }
+
+    if (bme280_convertCalibData(dev) != BME280_STATUS_OK) {
+        dev->status = BME280_STATUS_ERROR;
+        return BME280_STATUS_ERROR;
     }
 
     return BME280_STATUS_OK;
 }
 
-bme280_status_t bme280_read_raw(bme280_t* dev, bme280_raw_t* raw) {
-    memset(dev->_bme280HUMIRegister, 0, sizeof(dev->_bme280HUMIRegister));
-    memset(dev->_bme280PRESRegister, 0, sizeof(dev->_bme280PRESRegister));
-    memset(dev->_bme280TEMPRegister, 0, sizeof(dev->_bme280TEMPRegister));
-
-    size_t i = 0;
-    while(i < BME280_HUMIDITY_SIZE) {
-        uint8_t addr = (uint8_t)(BME280_HUMI_LSB_ADDR + (uint8_t)i);
-        dev->_bme280HUMIRegister[i] = addr;
-        if (BME280_HUMI_MSB_ADDR == addr) {
-            break;
-        }
-        ++i;
+bme280_status_t bme280_set_config(bme280_t* dev, bme280_config_t cfg) {
+    bme280_bus_t* bus = &dev->bme280_bus;
+    if (!dev || !bus->read || !bus->write) {
+        return BME280_STATUS_INVALID_ARG;
     }
 
-    i = 0;
-    while(i < BME280_PRESS_SIZE) {
-        dev->_bme280PRESRegister[i] = (BME280_PRES_XLSB_ADDR + i);
-        uint8_t addr = (uint8_t)(BME280_HUMI_LSB_ADDR + (uint8_t)i);
-        if (BME280_TEMP_MSB_ADDR == addr) {
-            break;
-        }
-        ++i;
+    uint8_t ctrlMeas = 0;
+    if (bus->read(bus->dev_addr, BME280_CTRL_MEAS_ADDR, &ctrlMeas, 1, bus->ctx) != BME280_STATUS_OK) {
+        return BME280_STATUS_ERROR;
+    }
+    ctrlMeas = ((ctrlMeas & 0xFC) | (SLEEP_MODE & 0x03));
+    if (bus->write(bus->dev_addr, BME280_CTRL_MEAS_ADDR, ctrlMeas, 1, bus->ctx) != BME280_STATUS_OK) {
+        return BME280_STATUS_ERROR;
     }
 
-    i = 0;
-    while(i < BME280_TEMPERATURE_SIZE) {
-        uint8_t addr = (uint8_t)(BME280_HUMI_LSB_ADDR + (uint8_t)i);
-        dev->_bme280TEMPRegister[i] = (BME280_TEMP_XLSB_ADDR + i);
-        if (BME280_PRES_MSB_ADDR == addr) {
-            break;
-        }
-        ++i;
+    if (bus->write(bus->dev_addr, BME280_CONFIG_ADDR, (cfg.bme280_standby | cfg.bme280_filter | cfg.bme280_spi_status), 1, bus->ctx) != BME280_STATUS_OK) {
+        return BME280_STATUS_ERROR;
     }
+
+    ctrlMeas = ((ctrlMeas & 0xFC) | (dev->bme280_mode & 0x03));
+    if (bus->write(bus->dev_addr, BME280_CTRL_MEAS_ADDR, ctrlMeas, 1, bus->ctx) != BME280_STATUS_OK) {
+        return BME280_STATUS_ERROR;
+    }
+
+    return BME280_STATUS_OK;
+}
+
+bme280_status_t bme280_read_raw(bme280_t* dev) {
+    if (!dev || !dev->bme280_bus.read || !dev->bme280_bus.write) {
+        return BME280_STATUS_INVALID_ARG;
+    }
+
+    if (dev->bme280_mode == FORCED_MODE) {
+        uint8_t status = 0;
+        do {
+            if (dev->bme280_bus.read(dev->bme280_bus.dev_addr, BME280_STATUS_ADDR, &status, 1, dev->bme280_bus.ctx) != BME280_STATUS_OK) {
+                dev->status = BME280_STATUS_ERROR;
+                return BME280_STATUS_ERROR;
+            }
+
+        } while (status & (1u << BME280_STATUS_MEASURING));
+    }
+
+    const uint8_t len = BME280_HUMI_LSB_ADDR - BME280_PRES_MSB_ADDR + 1;
+    uint8_t bme280_data_raw[len];
+    if (dev->bme280_bus.read(dev->bme280_bus.dev_addr, BME280_PRES_MSB_ADDR, bme280_data_raw, len, dev->bme280_bus.ctx) != BME280_STATUS_OK) {
+        dev->status = BME280_STATUS_ERROR;
+        return BME280_STATUS_ERROR;
+    }
+
+    dev->bme280_raw.bme280_pressure_raw = (bme280_uint32_t)(((bme280_uint16_t)bme280_data_raw[0] << 12) | ((bme280_uint16_t)bme280_data_raw[1] << 4) | ((bme280_uint16_t)bme280_data_raw[2] >> 4));
+    dev->bme280_raw.bme280_temperature_raw = (bme280_uint32_t)(((bme280_uint16_t)bme280_data_raw[3] << 12) | ((bme280_uint16_t)bme280_data_raw[4] << 4) | ((bme280_uint16_t)bme280_data_raw[5] >> 4));
+    dev->bme280_raw.bme280_humidity_raw = (bme280_uint16_t)(((bme280_uint16_t)bme280_data_raw[6] << 8) | ((bme280_uint16_t)bme280_data_raw[7]));
 
     return BME280_STATUS_OK;
 }
@@ -83,38 +162,40 @@ bme280_status_t bme280_read_raw(bme280_t* dev, bme280_raw_t* raw) {
 bme280_status_t bme280_convertCalibData(bme280_t* dev) {
     bme280_calibration_data_t* calib_data = &dev->bme280_calib_data;
 
-    uint8_t* humi_calib_buf = dev->bme280_calib_buf.bme280_humi_calib_buf;
-    uint8_t* pres_calib_buf = dev->bme280_calib_buf.bme280_pres_calib_buf;
-    uint8_t* temp_calib_buf = dev->bme280_calib_buf.bme280_temp_calib_buf;
+    uint8_t* temp_calib_buf = dev->bme280_calib_buf;
+    uint8_t* pres_calib_buf = dev->bme280_calib_buf + BME280_TEMPERATURE_SIZE;
+    uint8_t* humi_calib_buf = dev->bme280_calib_buf + BME280_TEMPERATURE_SIZE + BME280_PRESS_SIZE;
 
-    calib_data->dig_T1 = (uint16_t)((temp_calib_buf[1] << 8) | temp_calib_buf[0]);
-    calib_data->dig_T2 = (int16_t)((temp_calib_buf[3] << 8) | temp_calib_buf[2]);
-    calib_data->dig_T3 = (int16_t)((temp_calib_buf[5] << 8) | temp_calib_buf[4]);
+    calib_data->dig_T1 = (uint16_t)(((uint16_t)temp_calib_buf[1] << 8) | (uint16_t)temp_calib_buf[0]);
+    calib_data->dig_T2 = (int16_t)(uint16_t)(((uint16_t)temp_calib_buf[3] << 8) | (uint16_t)temp_calib_buf[2]);
+    calib_data->dig_T3 = (int16_t)(uint16_t)(((uint16_t)temp_calib_buf[5] << 8) | (uint16_t)temp_calib_buf[4]);
 
-    calib_data->dig_P1 = (uint16_t)((pres_calib_buf[1]) << 8 | pres_calib_buf[0]);
-    calib_data->dig_P2 = (int16_t)((pres_calib_buf[3]) << 8 | pres_calib_buf[2]);
-    calib_data->dig_P3 = (int16_t)((pres_calib_buf[5]) << 8 | pres_calib_buf[4]);
-    calib_data->dig_P4 = (int16_t)((pres_calib_buf[7]) << 8 | pres_calib_buf[6]);
-    calib_data->dig_P5 = (int16_t)((pres_calib_buf[9]) << 8 | pres_calib_buf[8]);
-    calib_data->dig_P6 = (int16_t)((pres_calib_buf[11]) << 8 | pres_calib_buf[10]);
-    calib_data->dig_P7 = (int16_t)((pres_calib_buf[13]) << 8 | pres_calib_buf[12]);
-    calib_data->dig_P8 = (int16_t)((pres_calib_buf[15]) << 8 | pres_calib_buf[14]);
-    calib_data->dig_P9 = (int16_t)((pres_calib_buf[17]) << 8 | pres_calib_buf[16]);
+    calib_data->dig_P1 = (uint16_t)(((uint16_t)pres_calib_buf[1]) << 8 | (uint16_t)pres_calib_buf[0]);
+    calib_data->dig_P2 = (int16_t)(uint16_t)(((uint16_t)pres_calib_buf[3]) << 8 | (uint16_t)pres_calib_buf[2]);
+    calib_data->dig_P3 = (int16_t)(uint16_t)(((uint16_t)pres_calib_buf[5]) << 8 | (uint16_t)pres_calib_buf[4]);
+    calib_data->dig_P4 = (int16_t)(uint16_t)(((uint16_t)pres_calib_buf[7]) << 8 | (uint16_t)pres_calib_buf[6]);
+    calib_data->dig_P5 = (int16_t)(uint16_t)(((uint16_t)pres_calib_buf[9]) << 8 | (uint16_t)pres_calib_buf[8]);
+    calib_data->dig_P6 = (int16_t)(uint16_t)(((uint16_t)pres_calib_buf[11]) << 8 | (uint16_t)pres_calib_buf[10]);
+    calib_data->dig_P7 = (int16_t)(uint16_t)(((uint16_t)pres_calib_buf[13]) << 8 | (uint16_t)pres_calib_buf[12]);
+    calib_data->dig_P8 = (int16_t)(uint16_t)(((uint16_t)pres_calib_buf[15]) << 8 | (uint16_t)pres_calib_buf[14]);
+    calib_data->dig_P9 = (int16_t)(uint16_t)(((uint16_t)pres_calib_buf[17]) << 8 | (uint16_t)pres_calib_buf[16]);
 
     calib_data->dig_H1 = humi_calib_buf[0];
-    calib_data->dig_H2 = (int16_t)((humi_calib_buf[2] << 8) | humi_calib_buf[1]);
+    calib_data->dig_H2 = (int16_t)(uint16_t)(((uint16_t)humi_calib_buf[2] << 8) | (uint16_t)humi_calib_buf[1]);
     calib_data->dig_H3 = humi_calib_buf[3];
-    calib_data->dig_H4 = (int16_t)((humi_calib_buf[5] << 8) | humi_calib_buf[4]);
-    calib_data->dig_H5 = (int16_t)((humi_calib_buf[7] << 8) | humi_calib_buf[6]);
-    calib_data->dig_H6 = (int8_t)(humi_calib_buf[8]);
+    calib_data->dig_H4 = (int16_t)(uint16_t)(((uint16_t)humi_calib_buf[4] << 4) | (uint16_t)(humi_calib_buf[5] & 0x0F));
+    calib_data->dig_H5 = (int16_t)(uint16_t)(((uint16_t)humi_calib_buf[6] << 4) | (uint16_t)(humi_calib_buf[5] >> 4));
+    calib_data->dig_H6 = (int8_t)(humi_calib_buf[7]);
+
+    return BME280_STATUS_OK;
 }
 
-bme280_status_t bme280_read(bme280_t* dev, float* T, bme280_uint32_t* H, bme280_uint32_t* P) {
+bme280_status_t bme280_read(bme280_t* dev, bme280_uint32_t* T, bme280_uint32_t* H, bme280_uint32_t* P) {
     bme280_calibration_data_t* calib_data = &dev->bme280_calib_data;
     bme280_int32_t adc_T = dev->bme280_raw.bme280_temperature_raw;
     bme280_int32_t adc_P = dev->bme280_raw.bme280_pressure_raw;
     bme280_int32_t adc_H = dev->bme280_raw.bme280_humidity_raw;
-    bme280_int32_t var1, var2;
+    bme280_int64_t var1, var2;
 
     var1 = ((((adc_T >> 3) - ((bme280_int32_t)calib_data->dig_T1 << 1))) *
             ((bme280_int32_t)calib_data->dig_T2)) >> 11;
@@ -122,7 +203,7 @@ bme280_status_t bme280_read(bme280_t* dev, float* T, bme280_uint32_t* H, bme280_
             ((bme280_int32_t)calib_data->dig_T1))) >> 12) * ((bme280_int32_t)calib_data->dig_T3)) >> 14;
     bme280_int32_t t_fine = var1 + var2;
 
-    *T = (t_fine * 5 + 128) >> 8;
+    *T = (bme280_uint32_t)((t_fine * 5 + 128) >> 8) / 100.0f;
 
     var1 = ((bme280_int64_t)t_fine) - 128000;
     var2 = var1 * var1 * (bme280_int64_t)calib_data->dig_P6;
@@ -131,15 +212,15 @@ bme280_status_t bme280_read(bme280_t* dev, float* T, bme280_uint32_t* H, bme280_
     var1 = ((var1 * var1 * (bme280_int64_t)calib_data->dig_P3) >> 8) + ((var1 *
             (bme280_int64_t)calib_data->dig_P2) << 12);
     var1 = (((((bme280_int64_t)1) << 47) + var1)) * ((bme280_int64_t)calib_data->dig_P1) >> 33;
-    if(var1 == 0) {
-        return 0; // avoid exception caused by division by zero
+    if (var1 == 0) {
+        return BME280_STATUS_ERROR; // avoid exception caused by division by zero
     }
 
-    *P = 1048576 - adc_P;
-    *P = (((*P << 31) - var2) * 3125) / var1;
-    var1 = (((bme280_int64_t)calib_data->dig_P9) * (*P >> 13) * (*P >> 13)) >> 25;
-    var2 =(((bme280_int64_t)calib_data->dig_P8) * *P) >> 19;
-    *P = ((*P + var1 + var2) >> 8) + (((bme280_int64_t)calib_data->dig_P7) << 4);
+    bme280_int64_t p_fine = 1048576 - adc_P;
+    p_fine = (((p_fine << 31) - var2) * 3125) / var1;
+    var1 = (((bme280_int64_t)calib_data->dig_P9) * (p_fine >> 13) * (p_fine >> 13)) >> 25;
+    var2 =(((bme280_int64_t)calib_data->dig_P8) * p_fine) >> 19;
+    *P = ((p_fine + var1 + var2) >> 8) + (((bme280_int64_t)calib_data->dig_P7) << 4);
 
     bme280_int64_t v_x1_u32r;
     v_x1_u32r = (t_fine - ((bme280_int64_t)76800));
@@ -153,6 +234,52 @@ bme280_status_t bme280_read(bme280_t* dev, float* T, bme280_uint32_t* H, bme280_
     v_x1_u32r = (v_x1_u32r < 0 ? 0 : v_x1_u32r);
     v_x1_u32r = (v_x1_u32r > 419430400? 419430400: v_x1_u32r);
     *H = (bme280_uint32_t)(v_x1_u32r>>12);
+
+    return BME280_STATUS_OK;
+}
+
+bme280_status_t bme280_weatherMonitoring(bme280_t* dev, bme280_config_t* cfg) {
+    dev->bme280_mode = FORCED_MODE;
+    dev->bme280_pressure_ovs = BME280_PRESSURE_OVERSAMPLING_X1;
+    dev->bme280_temperature_ovs = BME280_TEMPERATURE_OVERSAMPLING_X1;
+    dev->bme280_humidity_ovs = BME280_HUMIDITY_OVERSAMPLING_X1;
+
+    cfg->bme280_filter = BME280_FILTER_OFF;
+
+    return BME280_STATUS_OK;
+}
+
+bme280_status_t bme280_humiditySensing(bme280_t* dev, bme280_config_t* cfg) {
+    dev->bme280_mode = FORCED_MODE;
+    dev->bme280_pressure_ovs = BME280_PRESSURE_OVERSAMPLING_SKIPPED;
+    dev->bme280_temperature_ovs = BME280_TEMPERATURE_OVERSAMPLING_X1;
+    dev->bme280_humidity_ovs = BME280_HUMIDITY_OVERSAMPLING_X1;
+
+    cfg->bme280_filter = BME280_FILTER_OFF;
+
+    return BME280_STATUS_OK;
+}
+
+bme280_status_t bme280_indoorNavigation(bme280_t* dev, bme280_config_t* cfg) {
+    dev->bme280_mode = NORMAL_MODE;
+    dev->bme280_pressure_ovs = BME280_PRESSURE_OVERSAMPLING_X16;
+    dev->bme280_temperature_ovs = BME280_TEMPERATURE_OVERSAMPLING_X2;
+    dev->bme280_humidity_ovs = BME280_HUMIDITY_OVERSAMPLING_X1;
+
+    cfg->bme280_standby = BME280_STANDBY_0_5;
+    cfg->bme280_filter = BME280_FILTER_16;
+
+    return BME280_STATUS_OK;
+}
+
+bme280_status_t bme280_gaming(bme280_t* dev, bme280_config_t* cfg) {
+    dev->bme280_mode = NORMAL_MODE;
+    dev->bme280_pressure_ovs = BME280_PRESSURE_OVERSAMPLING_X4;
+    dev->bme280_temperature_ovs = BME280_TEMPERATURE_OVERSAMPLING_X1;
+    dev->bme280_humidity_ovs = BME280_HUMIDITY_OVERSAMPLING_SKIPPED;
+
+    cfg->bme280_standby = BME280_STANDBY_0_5;
+    cfg->bme280_filter = BME280_FILTER_16;
 
     return BME280_STATUS_OK;
 }
--- Drivers/bme280/bme280.h
+++ Drivers/bme280/bme280.h
@@ -12,9 +12,11 @@
 #include "stdint.h"
 #include "stddef.h"
 
-#define BME280_HUMIDITY_SIZE (BME280_CALIB_HUMI_ADDR_END - BME280_CALIB_HUMI_ADDR_START)
-#define BME280_PRESS_SIZE (BME280_CALIB_PRES_ADDR_END - BME280_CALIB_PRES_ADDR_START)
-#define BME280_TEMPERATURE_SIZE (BME280_CALIB_TEMP_ADDR_END - BME280_CALIB_TEMP_ADDR_START)
+#define BME280_CALIB_HUMI_SIZE_START     (1u)
+#define BME280_CALIB_HUMI_SIZE_CONTINUE  (BME280_CALIB_HUMI_ADDR_END - BME280_CALIB_HUMI_ADDR_CONTINUE + 1u)
+#define BME280_HUMIDITY_SIZE (BME280_CALIB_HUMI_SIZE_START + BME280_CALIB_HUMI_SIZE_CONTINUE)
+#define BME280_PRESS_SIZE (BME280_CALIB_PRES_ADDR_END - BME280_CALIB_PRES_ADDR_START + 1u)
+#define BME280_TEMPERATURE_SIZE (BME280_CALIB_TEMP_ADDR_END - BME280_CALIB_TEMP_ADDR_START + 1u)
 
 typedef int8_t bme280_int8_t;
 typedef uint8_t bme280_uint8_t;
@@ -24,18 +26,21 @@ typedef int32_t bme280_int32_t;
 typedef uint32_t bme280_uint32_t;
 typedef int64_t bme280_int64_t;
 
-const uint8_t BME280_RESET = 0xB6;
+static const uint8_t BME280_RESET = 0xB6;
+static const uint8_t BME280_ID = 0x60;
 
 typedef enum {
     BME280_STATUS_OK,
     BME280_STATUS_RESET,
-    BME280_STATUS_ERROR
+    BME280_STATUS_ERROR,
+    BME280_STATUS_INVALID_ARG,
+    BME280_STATUS_INVALID_STATE
 } bme280_status_t;
 
 typedef enum {
-    SLEEP_MODE = 0b00,
-    FORCED_MODE = 0b01,
-    NORMAL_MODE = 0b11
+    SLEEP_MODE = (0b00 << BME280_MODE),
+    FORCED_MODE = (0b01 << BME280_MODE),
+    NORMAL_MODE = (0b11 << BME280_MODE)
 } bme280_mode_t;
 
 typedef enum {
@@ -50,11 +55,11 @@ typedef enum {
 } bme280_standby_t;
 
 typedef enum {
-    BME280_FILTER_OFF = (0b000 << BME280_CONFIG_FILER),
-    BME280_FILTER_2 = (0b001 << BME280_CONFIG_FILER),
-    BME280_FILTER_4 = (0b010 << BME280_CONFIG_FILER),
-    BME280_FILTER_8 = (0b011 << BME280_CONFIG_FILER),
-    BME280_FILTER_16 = (0b100 << BME280_CONFIG_FILER)
+    BME280_FILTER_OFF = (0b000 << BME280_CONFIG_FILTER),
+    BME280_FILTER_2 = (0b001 << BME280_CONFIG_FILTER),
+    BME280_FILTER_4 = (0b010 << BME280_CONFIG_FILTER),
+    BME280_FILTER_8 = (0b011 << BME280_CONFIG_FILTER),
+    BME280_FILTER_16 = (0b100 << BME280_CONFIG_FILTER)
 } bme280_filter_t;
 
 typedef enum {
@@ -63,37 +68,31 @@ typedef enum {
 } bme280_spi_status_t;
 
 typedef enum {
-    BME280_HUMIDITY_OVERSAMPLING_SKIPPED = 0b000,
-    BME280_HUMIDITY_OVERSAMPLING_X1 = 0b001,
-    BME280_HUMIDITY_OVERSAMPLING_X2 = 0b010,
-    BME280_HUMIDITY_OVERSAMPLING_X4 = 0b011,
-    BME280_HUMIDITY_OVERSAMPLING_X8 = 0b100,
-    BME280_HUMIDITY_OVERSAMPLING_X16 = 0b101,
+    BME280_HUMIDITY_OVERSAMPLING_SKIPPED = (0b000 << BME280_HUMI_OVS),
+    BME280_HUMIDITY_OVERSAMPLING_X1 = (0b001 << BME280_HUMI_OVS),
+    BME280_HUMIDITY_OVERSAMPLING_X2 = (0b010 << BME280_HUMI_OVS),
+    BME280_HUMIDITY_OVERSAMPLING_X4 = (0b011 << BME280_HUMI_OVS),
+    BME280_HUMIDITY_OVERSAMPLING_X8 = (0b100 << BME280_HUMI_OVS),
+    BME280_HUMIDITY_OVERSAMPLING_X16 = (0b101 << BME280_HUMI_OVS)
 } bme280_humidity_ovs_t;
 
 typedef enum {
-    BME280_PRESSURE_OVERSAMPLING_SKIPPED = 0b000,
-    BME280_PRESSURE_OVERSAMPLING_X1 = 0b001,
-    BME280_PRESSURE_OVERSAMPLING_X2 = 0b010,
-    BME280_PRESSURE_OVERSAMPLING_X4 = 0b011,
-    BME280_PRESSURE_OVERSAMPLING_X8 = 0b100,
-    BME280_PRESSURE_OVERSAMPLING_X16 = 0b101,
+    BME280_PRESSURE_OVERSAMPLING_SKIPPED = (0b000 << BME280_PRES_OVS),
+    BME280_PRESSURE_OVERSAMPLING_X1 = (0b001 << BME280_PRES_OVS),
+    BME280_PRESSURE_OVERSAMPLING_X2 = (0b010 << BME280_PRES_OVS),
+    BME280_PRESSURE_OVERSAMPLING_X4 = (0b011 << BME280_PRES_OVS),
+    BME280_PRESSURE_OVERSAMPLING_X8 = (0b100 << BME280_PRES_OVS),
+    BME280_PRESSURE_OVERSAMPLING_X16 = (0b101 << BME280_PRES_OVS)
 } bme280_pressure_ovs_t;
 
 typedef enum {
-    BME280_TEMPERATURE_OVERSAMPLING_SKIPPED = 0b000,
-    BME280_TEMPERATURE_OVERSAMPLING_X1 = 0b001,
-    BME280_TEMPERATURE_OVERSAMPLING_X2 = 0b010,
-    BME280_TEMPERATURE_OVERSAMPLING_X4 = 0b011,
-    BME280_TEMPERATURE_OVERSAMPLING_X8 = 0b100,
-    BME280_TEMPERATURE_OVERSAMPLING_X16 = 0b101,
+    BME280_TEMPERATURE_OVERSAMPLING_SKIPPED = (0b000 << BME280_TEMP_OVS),
+    BME280_TEMPERATURE_OVERSAMPLING_X1 = (0b001 << BME280_TEMP_OVS),
+    BME280_TEMPERATURE_OVERSAMPLING_X2 = (0b010 << BME280_TEMP_OVS),
+    BME280_TEMPERATURE_OVERSAMPLING_X4 = (0b011 << BME280_TEMP_OVS),
+    BME280_TEMPERATURE_OVERSAMPLING_X8 = (0b100 << BME280_TEMP_OVS),
+    BME280_TEMPERATURE_OVERSAMPLING_X16 = (0b101 << BME280_TEMP_OVS)
 } bme280_temperature_ovs_t;
-
-typedef struct {
-    uint8_t bme280_humi_calib_buf[BME280_HUMIDITY_SIZE];
-    uint8_t bme280_pres_calib_buf[BME280_PRESS_SIZE];
-    uint8_t bme280_temp_calib_buf[BME280_TEMPERATURE_SIZE];
-} bme280_calibration_buf_t;
 
 typedef struct {
     bme280_uint16_t dig_T1;
@@ -130,29 +129,59 @@ typedef struct {
     bme280_spi_status_t bme280_spi_status;
 } bme280_config_t;
 
+typedef bme280_status_t (*bme280_read_fn_t)(
+        uint8_t dev_addr,
+        uint8_t reg_addr,
+        uint8_t* data,
+        uint8_t len,
+        void* ctx
+);
+
+typedef bme280_status_t (*bme280_write_fn_t)(
+        uint8_t dev_addr,
+        uint8_t reg_addr,
+        const uint8_t* data,
+        uint8_t len,
+        void* ctx
+);
+
 typedef struct {
+    bme280_read_fn_t read;
+    bme280_write_fn_t write;
+    void* ctx;
+    uint8_t dev_addr;
+} bme280_bus_t;
+
+typedef struct {
+    bme280_bus_t bme280_bus;
+
     bme280_status_t status;
     bme280_uint8_t reset;
 
+    bme280_mode_t bme280_mode;
     bme280_humidity_ovs_t bme280_humidity_ovs;
     bme280_pressure_ovs_t bme280_pressure_ovs;
     bme280_temperature_ovs_t bme280_temperature_ovs;
 
-    bme280_uint16_t _bme280HUMIRegister[BME280_HUMIDITY_SIZE];
-    bme280_uint16_t _bme280PRESRegister[BME280_PRESS_SIZE];
-    bme280_uint16_t _bme280TEMPRegister[BME280_TEMPERATURE_SIZE];
-
-    bme280_calibration_buf_t bme280_calib_buf;
+    uint8_t bme280_calib_buf[BME280_HUMIDITY_SIZE + BME280_PRESS_SIZE + BME280_TEMPERATURE_SIZE];
     bme280_calibration_data_t bme280_calib_data;
     bme280_raw_t bme280_raw;
 } bme280_t;
 
 bme280_status_t bme280_init(bme280_t* dev, const bme280_bus_t* bus);
 bme280_status_t bme280_reset(bme280_t* dev);
+bme280_status_t bme280_set_mode(bme280_t* dev, bme280_mode_t mode);
 
 bme280_status_t bme280_read_calibration(bme280_t* dev);
 bme280_status_t bme280_set_config(bme280_t* dev, bme280_config_t cfg);
-bme280_status_t bme280_read_raw(bme280_t* dev, bme280_raw_t* raw);
-bme280_status_t bme280_read(bme280_t* dev, float* T, bme280_uint32_t* H, bme280_uint32_t* P);
+bme280_status_t bme280_read_raw(bme280_t* dev);
+bme280_status_t bme280_read(bme280_t* dev, bme280_uint32_t* T, bme280_uint32_t* H, bme280_uint32_t* P);
+bme280_status_t bme280_convertCalibData(bme280_t* dev);
+
+bme280_status_t bme280_weatherMonitoring(bme280_t* dev, bme280_config_t* cfg);
+bme280_status_t bme280_humiditySensing(bme280_t* dev, bme280_config_t* cfg);
+bme280_status_t bme280_indoorNavigation(bme280_t* dev, bme280_config_t* cfg);
+bme280_status_t bme280_gaming(bme280_t* dev, bme280_config_t* cfg);
+
 
 #endif /* BME280_BME280_H_ */
